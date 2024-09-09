382f26b1e14e8edadbc4a0ef42c12ffd83ea535e
Dainv <daicv30072003@gmail.com>
2024-09-09 17:52:01 +0700

    update service pre-processing and API import in service-temp-manage-db

--- service-pre-processing/main.py
+++ service-pre-processing/main.py
@@ -1,74 +1,101 @@
+from flask import Flask, jsonify, request
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import pymongo
 from config import Config
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
-from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
 
 client = pymongo.MongoClient(Config.MONGO_URL)
-db = client.PRE
+db = client.HDD
 collection = db.Heart_Disease_Data
 
-data = list(collection.find())
-df = pd.DataFrame(data)
-df = df.drop(columns=['_id'])
 
-# Xem trước vài dòng đầu của dữ liệu
-print(df.head())
+@app.route('/import-mongodb', methods=['POST'])
+def import_mongodb():
+    try:
+        data = request.get_json()
+        result = collection.insert_many(data)
+        res = len(result.inserted_ids)
+        return jsonify({"message": "Success", "records_inserted": res})
+    except Exception as e:
+        return jsonify({"message": "Đã xảy ra lỗi", "error": str(e)})
 
-# Kiểm tra thông tin về dữ liệu
-print(df.info())
+@app.route('/pre-processing', methods=['POST'])
+def pre_processing():
+    try:
+        data = request.get_json()
+        res = do_pre_processing(data)
+        return jsonify(res) 
+    except Exception as e:
+        return jsonify({"message": "Đã xảy ra lỗi", "error": str(e)})
+    
+def do_pre_processing(data):
+    df = pd.DataFrame(data)
+    # Kiểm tra thông tin về dữ liệu
+    print(df.info())
+    # Xem trước vài dòng đầu của dữ liệu
+    print(df.head())
 
-print("Số lượng dữ liệu trước khi xử lý:", len(df))
+    print("Số lượng dữ liệu trước khi xử lý:", len(df))
+    
+    #Null
+    df = df.dropna()
 
-#Null
-df = df.dropna()
+    #Age
+    df = df[(df['age'] >= 29) & (df['age'] <= 77)]
 
-#Age
-df = df[(df['age'] >= 29) & (df['age'] <= 77)]
+    #Sex
+    df = df[(df['sex'] >= 0) & (df['sex'] <= 1)]
 
-#Sex
-df = df[(df['sex'] >= 0) & (df['sex'] <= 1)]
+    #Chest Pain Type
+    df = df[(df['cp'] >= 0) & (df['cp'] <= 3)]
 
-#Chest Pain Type
-df = df[(df['cp'] >= 0) & (df['cp'] <= 3)]
+    #Resting Blood Pressure
+    df = df[(df['trestbps'] >= 80) & (df['trestbps'] <= 250)]
 
-#Resting Blood Pressure
-df = df[(df['trestbps'] >= 80) & (df['trestbps'] <= 250)]
+    #Serum Cholesterol in mg/dl
+    df = df[(df['chol'] >= 126) & (df['chol'] <= 564)]
 
-#Serum Cholesterol in mg/dl
-df = df[(df['chol'] >= 126) & (df['chol'] <= 564)]
+    #Fasting Blood Sugar > 120 mg/dl
+    df = df[(df['fbs'] >= 0) & (df['fbs'] <= 1)]
 
-#Fasting Blood Sugar > 120 mg/dl
-df = df[(df['fbs'] >= 0) & (df['fbs'] <= 1)]
+    #Resting Electrocardiographic Results
+    df = df[(df['restecg'] >= 0) & (df['restecg'] <= 2)]
 
-#Resting Electrocardiographic Results
-df = df[(df['restecg'] >= 0) & (df['restecg'] <= 2)]
+    #Maximum Heart Rate Achieved 
+    df = df[(df['thalach'] >= 71) & (df['thalach'] <= 202)]
 
-#Maximum Heart Rate Achieved 
-df = df[(df['thalach'] >= 71) & (df['thalach'] <= 202)]
+    #Exercise Induced Angina
+    df = df[(df['exang'] >= 0) & (df['exang'] <= 1)]
 
-#Exercise Induced Angina
-df = df[(df['exang'] >= 0) & (df['exang'] <= 1)]
+    #Oldpeak (ST Depression Induced by Exercise Relative to Rest)
+    df = df[(df['oldpeak'] >= 0) & (df['oldpeak'] <= 6.2)]
 
-#Oldpeak (ST Depression Induced by Exercise Relative to Rest)
-df = df[(df['oldpeak'] >= 0) & (df['oldpeak'] <= 6.2)]
+    #The Slope of the Peak Exercise ST Segment
+    df = df[(df['slope'] >= 0) & (df['slope'] <= 2)]
 
-#The Slope of the Peak Exercise ST Segment
-df = df[(df['slope'] >= 0) & (df['slope'] <= 2)]
+    #Number of Major Vessels Colored by Fluoroscopy
+    df = df[(df['ca'] >= 0) & (df['ca'] <= 3)]
 
-#Number of Major Vessels Colored by Fluoroscopy
-df = df[(df['ca'] >= 0) & (df['ca'] <= 3)]
+    #Thal (Thallium Stress Test Result)
+    df = df[(df['thal'] >= 1) & (df['thal'] <= 3)]
 
-#Thal (Thallium Stress Test Result)
-df = df[(df['thal'] >= 1) & (df['thal'] <= 3)]
+    print("Số lượng dữ liệu sau khi xử lý:", len(df))
 
-print("Số lượng dữ liệu sau khi xử lý:", len(df))
+    # Chuẩn hóa các cột giá trị liên tục
+    scaler = MinMaxScaler()
+    continuous_columns = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+    df[continuous_columns] = scaler.fit_transform(df[continuous_columns])
+    print(df.head())
 
-# Chuẩn hóa các cột giá trị liên tục
-scaler = MinMaxScaler()
-continuous_columns = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
-df[continuous_columns] = scaler.fit_transform(df[continuous_columns])
+    return df.to_dict(orient='records')
 
-print(df.head())
+@app.route('/')
+def index():
+    return "This is Temp-Import-DB Service"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8033)
 
 
