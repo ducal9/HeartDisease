fffa853ef6d29154e71ea1dee85a5060eef1c696
Dainv <daicv30072003@gmail.com>
2024-10-15 10:12:11 +0700

    Delete mongo

--- service-email/main.py
+++ service-email/main.py
@@ -85,27 +85,6 @@ def get_email():
     return jsonify(subjects)
 
 
-@app.route('/import-db', methods=['POST'])
-def import_db():
-    try:
-        file = request.files['file']
-        df = pd.read_csv(file)
-        
-        data = df.to_dict(orient='records')
-        url_pre = Config.SERVICE_PRE_PROCESSING + "/pre-processing"
-
-        res_pre = requests.post(url_pre, json=data)
-        if res_pre.status_code == 200:
-            try:
-                data = res_pre.json()
-                url_import = Config.SERVICE_PRE_PROCESSING + "/import-mongodb"
-                res_import = requests.post(url_import, json=data)
-                return jsonify(res_import.json())
-            except ValueError:
-                return res_pre.text
-    except Exception as e:
-        return jsonify({"message": "Đã xảy ra lỗi", "error": str(e)})
-
 @app.route('/')
 def index():
     return "This is Email Service"
